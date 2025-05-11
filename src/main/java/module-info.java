23811e32a213f6843c8c67a522da911b6418da87
ingabireol <ingabire069@gmail.com>
2025-05-11 23:52:44 +0200

    debugging module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,17 @@
 module olim.com.hospitalmanagementsystem {
+    // JavaFX dependencies
     requires javafx.controls;
     requires javafx.fxml;
+
+    // JDBC
     requires java.sql;
 
+    // Open packages to FXML
+    opens olim.com.hospitalmanagementsystem.view to javafx.fxml;
+    opens olim.com.hospitalmanagementsystem.model to javafx.base;
 
-    opens olim.com.hospitalmanagementsystem to javafx.fxml;
+    // Export packages
     exports olim.com.hospitalmanagementsystem;
+    exports olim.com.hospitalmanagementsystem.view;
+    exports olim.com.hospitalmanagementsystem.model;
 }
